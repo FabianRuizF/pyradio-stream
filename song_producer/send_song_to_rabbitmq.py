b49b4efd3f4a5e56a91a6f22ed1c5064e8163506
FabianRuizF <fabiancrruiz@gmail.com>
2023-03-25 19:33:07 +0000

    Added audio read and json decode

--- song_producer/send_song_to_rabbitmq.py
+++ song_producer/send_song_to_rabbitmq.py
@@ -11,6 +11,14 @@ RABBITMQ_IP = ""
 RABBITMQ_PASS = "a_simple_user"
 RABBITMQ_USER = "a_simple_user"
 
+import base64
+import requests
+
+
+filepath = "/root/pyradio-stream/song_producer/b_slipout.mp3"
+with open(filepath, 'rb') as f:
+    audio_encoded = base64.b64encode(f.read())
+
 parameters = pika.URLParameters(f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_IP}:5672/%2f')
 pika_connection = pika.BlockingConnection(parameters)
 channel = pika_connection.channel()
@@ -19,6 +27,6 @@ exchange_name = 'song_exchange'
 queue_name = 'song_queue'
 
 channel.basic_publish(exchange_name,queue_name,
-                                json.dumps("test"),
+                                json.dumps(audio_encoded.decode()),
                                 pika.BasicProperties(delivery_mode=2))
 
