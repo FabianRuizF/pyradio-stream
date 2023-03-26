76f2e695a09c6d10d242c46a3e7fb1a2ec56170a
FabianRuizF <fabiancrruiz@gmail.com>
2023-03-26 16:49:21 +0000

    stream audio working

--- song_consumer/stream_audio.py
+++ song_consumer/stream_audio.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, Response
 from io import BytesIO
 
 from flask import Flask, Response
@@ -13,6 +14,22 @@ import requests
 
 app = Flask(__name__)
 
+def genHeader(sampleRate, bitsPerSample, channels):
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
 
 @app.route("/wav")
 def streamwav():
@@ -44,5 +61,73 @@ def streamwav():
 
 
 
+@app.route('/audio')
+def audio():
+    # start Recording
+    def sound():
+
+        #wav_header = genHeader(sampleRate, bitsPerSample, channels)
+
+
+        RABBITMQ_IP = ""
+        RABBITMQ_PASS = "a_simple_user"
+        RABBITMQ_USER = "a_simple_user"
+
+
+
+        parameters = pika.URLParameters(f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_IP}:5672/%2f')
+        pika_connection = pika.BlockingConnection(parameters)
+        channel = pika_connection.channel()
+
+        exchange_name = 'song_exchange'
+        queue_name = 'song_queue'
+
+
+        _,_,body = channel.basic_get(queue=queue_name,auto_ack=True)
+        message_json = json.loads(body.decode())
+        decode_string = base64.b64decode(message_json)
+        print(type(decode_string))
+        data_audio = BytesIO(decode_string)
+
+
+        CHUNK = 1024
+#        sampleRate = 44100
+#        bitsPerSample = 16
+#        channels = 1
+
+
+        sampleRate = 44100
+        bitsPerSample = 16
+        channels = 1
+
+        wav_header = genHeader(sampleRate, bitsPerSample, channels)
+        #data = data_audio.read(1024)
+
+
+        first_run = True
+        while True:
+           if first_run:
+               data = wav_header + data_audio.read(CHUNK)
+               first_run = False
+           else:
+               data = data_audio.read(1024)
+           print(len(data))
+           if(len(data)==0):
+               _,_,body = channel.basic_get(queue=queue_name,auto_ack=True)
+               message_json = json.loads(body.decode())
+               decode_string = base64.b64decode(message_json)
+               print(type(decode_string))
+               data_audio = BytesIO(decode_string)
+               data = data_audio.read(1024)
+           yield(data)
+    return Response(sound())
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('./index.html')
+
+
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0",threaded=True)
--- song_producer/send_song_to_rabbitmq.py
+++ song_producer/send_song_to_rabbitmq.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import pika
 import json
 import os
@@ -14,11 +14,16 @@ RABBITMQ_USER = "a_simple_user"
 import base64
 import requests
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
-filepath = "/root/pyradio-stream/song_producer/b_slipout.mp3"
+
+filepath = "/root/pyradio-stream/song_producer/tmp.wav"
 with open(filepath, 'rb') as f:
-    audio_encoded = base64.b64encode(f.read())
-
+#    audio_encoded = base64.b64encode(f.read())
+    audio_encoded = f.read()
 parameters = pika.URLParameters(f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_IP}:5672/%2f')
 pika_connection = pika.BlockingConnection(parameters)
 channel = pika_connection.channel()
@@ -26,7 +31,17 @@ channel = pika_connection.channel()
 exchange_name = 'song_exchange'
 queue_name = 'song_queue'
 
-channel.basic_publish(exchange_name,queue_name,
-                                json.dumps(audio_encoded.decode()),
-                                pika.BasicProperties(delivery_mode=2))
+import struct
 
+
+for a in range(300):
+    part = audio_encoded[44100*2*(a):44100*2*(a+1)]
+    binary_part= base64.b64encode(part)
+    channel.basic_publish(exchange_name,queue_name
+                                    json.dumps(binary_part.decode()),
+                                    pika.BasicProperties(delivery_mode=2))
+
+
+#channel.basic_publish(exchange_name,queue_name,
+#                                json.dumps(audio_encoded.decode()),
+#                                pika.BasicProperties(delivery_mode=2))
