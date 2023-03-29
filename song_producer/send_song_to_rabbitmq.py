1db3eafae0addef1915f6592580cf47a772a65b2
FabianRuizF <fabiancrruiz@gmail.com>
2023-03-29 18:51:22 +0000

    sending from rabbitmq queue to song_consumer, to be able to upload files and send them constantly

--- song_producer/send_song_to_rabbitmq.py
+++ song_producer/send_song_to_rabbitmq.py
@@ -1,72 +1,79 @@
-
-import numpy as np
+import soundfile as sf
+from pydub import AudioSegment
+import pyaudio
+import base64
+import io
+import time
+import wave
+import librosa
+import soundfile as sf
 import pika
 import json
-import os
-import rabbitpy
-
-import time
-
-import pyaudio
-import wave
-
-
-
 RABBITMQ_IP = ""
 RABBITMQ_PASS = "a_simple_user"
 RABBITMQ_USER = "a_simple_user"
 
-import base64
-import requests
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
 
-#change to get audio from flask service
-filepath = "/root/pyradio-stream/song_producer/tmp_v2.wav"
-with open(filepath, 'rb') as f:
-#    audio_encoded = base64.b64encode(f.read())
-    audio_encoded = f.read()
 parameters = pika.URLParameters(f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_IP}:5672/%2f')
 pika_connection = pika.BlockingConnection(parameters)
 channel = pika_connection.channel()
 
-exchange_name = 'song_exchange'
-queue_name = 'song_queue'
 
-import struct
+exchange_name = 'song_exchange_transformation'
+queue_name = "song_transformation_queue"
+
+while(True):
+    exchange_name = 'song_exchange_transformation'
+    queue_name = "song_transformation_queue"
+    _,_,body = channel.basic_get(queue=queue_name,auto_ack=True)
+    while(body is None):
+        print("waiting")
+        time.sleep(1)
+        _,_,body = channel.basic_get(queue=queue_name,auto_ack=True)
+
+
+    body = body.decode()
+    body = json.loads(body)
+    decode_string = base64.b64decode(body)
+
+    with open("temp.audio", 'wb') as f: 
+        f.write(decode_string)
 
 
 
-wf = wave.open(filepath, 'rb')
-import time, os, sys, contextlib
+    x,_ = librosa.load("temp.audio",sr=22050)
+    sf.write('tmp.wav', x, 22050)
 
-#@contextlib.contextmanager
-bit_len = wf.getsampwidth() * 8
-channels = wf.getnchannels()
-rate = int(wf.getframerate())
+    with open('tmp.wav', 'rb') as f:
+        audio_encoded = f.read()
 
-json_to_send = {}
-json_to_send["bit_len"] = bit_len
-json_to_send["channels"] =channels
-json_to_send["rate"] =rate
-print(len(audio_encoded)/rate/2)
-print(json_to_send)
-for a in range(30):
-    #psuedo code, change to get the final inde
-    if(a=="finish encode"):
-        part = audio_encoded[rate*2*(a):]
-    else:
-        part = audio_encoded[rate*2*(a):rate*2*(a+1)]
-    binary_part= base64.b64encode(part)
-    json_to_send["data"] = binary_part.decode()
-    channel.basic_publish(exchange=exchange_name, routing_key="",
-                                    body=json.dumps(json_to_send),
-                                    properties=pika.BasicProperties(delivery_mode=2))
-    time.sleep(0.9)
 
-#channel.basic_publish(exchange_name,queue_name,
-#                                json.dumps(audio_encoded.decode()),
-#                                pika.BasicProperties(delivery_mode=2))
+
+    wf = wave.open('tmp.wav', 'rb') 
+    p = pyaudio.PyAudio()
+
+    bit_len = wf.getsampwidth() * 8
+    channels = wf.getnchannels()
+    rate = int(wf.getframerate())
+
+    json_to_send = {}
+    json_to_send["bit_len"] = bit_len
+    json_to_send["channels"] =channels
+    json_to_send["rate"] =rate
+    print(len(audio_encoded)/rate/2)
+
+    exchange_name = 'song_exchange'
+    queue_name = 'song_queue'
+
+    for a in range(30):
+        if(a=="finish encode"):
+            part = audio_encoded[rate*2*(a):]
+        else:
+            part = audio_encoded[rate*2*(a):rate*2*(a+1)]
+        binary_part= base64.b64encode(part)
+        json_to_send["data"] = binary_part.decode()
+        channel.basic_publish(exchange=exchange_name, routing_key="",
+                                        body=json.dumps(json_to_send),
+                                        properties=pika.BasicProperties(delivery_mode=2))
+        time.sleep(0.9)
